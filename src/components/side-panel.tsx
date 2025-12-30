713e5028ea0b1f99fd4eae3455069658d4888abf
Lukas Wiku <ronkcloud@gmail.com>
2025-12-30 09:57:46 +0700

    side-panel thing

--- src/components/side-panel.tsx
+++ src/components/side-panel.tsx
@@ -1,15 +1,17 @@
-import { useState } from "react"
+import { useState } from "react";
+import { PanelLeft } from "lucide-react";
 
 export function SidePanel() {
     const [isExpand, setIsExpand] = useState(false);
 
     return (
-        <div className={`bg-bg-light ${isExpand ? 'min-w-48' : 'min-w-12'}`}>
-            <div 
-                className="bg-primary w-4 h-4 rounded-sm m-4 cursor-pointer"
-                onClick={() => setIsExpand(!isExpand)}    
+        <div className={`bg-bg-light ${isExpand? 'min-w-48': 'min-w-12'}`}>
+            <div
+                className="flex items-center justify-center bg-bg-light m-2 w-8 h-8 cursor-pointer rounded-xl hover:bg-highlight transition-all active:scale-90"
+                onClick={() => setIsExpand(!isExpand)}
             >
+                <PanelLeft size={20}/>
             </div>
-        </div>
+        </div>   
     )
 }
